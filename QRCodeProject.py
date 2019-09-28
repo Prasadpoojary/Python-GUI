ba888c1e216e158edfa7240428a479f4bcdbb6fd
Prasad <55648560+Prasad815@users.noreply.github.com>
2019-09-28 18:21:45 +0530

    Update QRCodeProject.py

--- QRCodeProject.py
+++ QRCodeProject.py
@@ -1,15 +1,21 @@
+#It will gives you a QRcode for your URL
+#to install tkinter and pyqrcode
+# pip install tkinter
+# pip install pyqrcode
+
+#import all require modules
 from tkinter import *
 from tkinter import filedialog
 import pyqrcode
 from pyqrcode import QRCode
 from tkinter import messagebox as mb
-
+#initialize Tk to window as object of tkinter
 window=Tk()
 window.title("QRCode Generator from Prasad Poojary ")
 window.geometry("600x450+350+100")
 window.configure(bg="#960018")
 
-
+# function will called when you hit the submit button
 def generatebtnclick():
     s=ent1.get()
     if len(s) == 0:
@@ -20,6 +26,7 @@ def generatebtnclick():
             m=f[53:]
             qrcode.svg(str(m)+".html",scale=10)
             mb.showinfo("Succuss","QR Code Succussfully Generated")
+# function will called when you hit the enter button as event
 def generateqr(event):
     s=ent1.get()
     if len(s) == 0:
@@ -31,7 +38,7 @@ def generateqr(event):
             qrcode.svg(str(m)+".html",scale=10)
             mb.showinfo("Succuss","QR Code Succussfully Generated")
 
- 
+# all user interface designs
 label1=Label(window,text="QR Code Generator",font="TimesNewRoman 30 bold",bg="#960018",fg="white")
 label1.place(x=145,y=100)
 label2=Label(window,text="Enter Link : ",font="arial 17",bg="#960018",fg="white")
